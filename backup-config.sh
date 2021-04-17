800bb6920622843e0f890daedd1d3ae5959bb235
garbray <bryangarzonbahamon@Bryans-MacBook-Pro.local>
2021-04-17 11:45:30 -0500

    chore: move files to a better named folder:
    - create install.sh
    - update backup-config file

--- backup-config.sh
+++ backup-config.sh
@@ -5,26 +5,26 @@ echo "$FGRN coping config files on the repo"
 repoPath="$PWD"
 
 #copy nvim
-cp -R ~/.config/nvim $repoPath/files/.config
+cp -R ~/.config/nvim $repoPath/dotfiles/.config
 echo "$FGRN copy nivm file"
 
 #copy tmux
-cp ~/.tmux.conf $repoPath/files
+cp ~/.tmux.conf $repoPath/dotfiles
 echo "$FGRN copy tmux config file"
 
 #copy zshrc
-cp ~/.zshrc $repoPath/files
+cp ~/.zshrc $repoPath/dotfiles
 echo "$FGRN copy zshrc file"
 
 #copy zhrc alias
-cp -R ~/.config/zsh $repoPath/files/.config
+cp -R ~/.config/zsh $repoPath/dotfiles/.config
 echo "$FGRN copy zsh import file"
 
 #copy kitty config
-cp -R ~/.config/kitty $repoPath/files/.config
+cp -R ~/.config/kitty $repoPath/dotfiles/.config
 echo "$FGRN copy kitty file"
 
 #copy tmux config
-cp -R ~/.config/tmux $repoPath/files/.config
+cp -R ~/.config/tmux $repoPath/dotfiles/.config
 echo "$FGRN copy tmux file"
 echo "$FGRN COPY DONE"
