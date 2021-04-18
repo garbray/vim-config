d6db21e6c78d3b676097c76fc48a393e32118023
Bryan Garzon <bryan.garzon@contractor.hallmark.com>
2021-04-18 12:06:08 -0500

    chore: add miss lsp servers
    Enable the following servers
    - bash
    - go
    - html
    - lua
    - python
    - rust
    - terraform
    - vim
    - yaml

--- dotfiles/.config/nvim/lua/garbray/lsp-servers/python.lua
+++ dotfiles/.config/nvim/lua/garbray/lsp-servers/python.lua
@@ -1,6 +1,5 @@
 -- npm i -g pyright
 require'lspconfig'.pyright.setup {
-    -- cmd = {DATA_PATH .. "/lspinstall/python/node_modules/.bin/pyright-langserver", "--stdio"},
     on_attach = require'garbray.config'.common_on_attach,
     -- handlers = {
     --     ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
--- dotfiles/.config/nvim/lua/garbray/lsp-servers/terraform.lua
+++ dotfiles/.config/nvim/lua/garbray/lsp-servers/terraform.lua
@@ -1,4 +1,3 @@
-require'lspconfig'.rust_analyzer.setup{
+require'lspconfig'.terraformls.setup{
     on_attach = require'garbray.config'.common_on_attach
 }
-
