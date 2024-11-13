aad817f824c3f33528c61dfd9ef5cf75604117b9
Bryan Garzon <bgarzon@atlassian.com>
2024-11-13 07:35:28 -0500

    update dependencies

--- dotfiles/.config/nvim/lua/garbray/pluginsList.lua
+++ dotfiles/.config/nvim/lua/garbray/pluginsList.lua
@@ -43,6 +43,15 @@ require("lazy").setup({
 		build = ":TSUpdate",
 	},
 	-- plugins
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 	"nvim-treesitter/playground",
 	"theprimeagen/harpoon",
 	"mbbill/undotree",
