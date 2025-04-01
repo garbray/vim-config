657a6efaddbd871f04fcf44813b327edf0664961
bryan garzon <bryangarzonb@gmail.com>
2025-04-01 18:43:41 -0500

    feat(global): introduce avante integration

--- dotfiles/.config/nvim/after/plugin/lsp-zero.lua
+++ dotfiles/.config/nvim/after/plugin/lsp-zero.lua
@@ -101,7 +101,7 @@ cmp.setup({
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("lspconfig").html.setup({
+lspconfig.html.setup({
 	capabilities = capabilities,
 })
 
