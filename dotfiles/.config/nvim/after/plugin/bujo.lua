6c946d80e5cd46e21b3fb7ebb98f6973f376d7c5
Bryan Garzon <bgarzon@atlassian.com>
2025-08-12 02:08:19 -0500

    fix(bujo): keymaps updates

--- dotfiles/.config/nvim/after/plugin/bujo.lua
+++ dotfiles/.config/nvim/after/plugin/bujo.lua
@@ -1,7 +1,7 @@
 -- vim  TODO pluggin
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>tt", ":Todo<CR>")
-keymap.set("n", "<leader>ct", "<Plug>BujoChecknormal")
-keymap.set("n", "<leader>at", "<Plug>BujoAddnormal")
+keymap.set("n", "<leader>bt", ":Todo<CR>", { desc = "Bujo Toggle Todo" })
+keymap.set("n", "<leader>bc", "<Plug>BujoChecknormal", { desc = "Bujo check Todo" })
+keymap.set("n", "<leader>ba", "<Plug>BujoAddnormal", { desc = "Add Todo" })
 vim.g["g#todo_file_path"] = '$HOME . "/.cache/bujo"'
